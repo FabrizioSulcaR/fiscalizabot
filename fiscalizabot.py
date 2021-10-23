f52e2df207de84a96624c66b89322df8295628e9
FabrizioSulcaR <78000967+FabrizioSulcaR@users.noreply.github.com>
2021-10-23 13:20:19 -0500

    Add files via upload

--- fiscalizabot.py
+++ fiscalizabot.py
@@ -26,7 +26,7 @@ class TweetBot:
         return line
     
     def post(self,data,i):
-        api.update_status('El presidente Pedro Castillo Terrones se reunió hoy, ' + str(data[i]['Fecha']) +', con ' + str(data[i]['Visitante'].title()) + ', ' + str(data[i]['Entidad']) + '.' + '\n' + 'La reunión se llevó a cabo desde las ' + str(data[i]['Hora_ingreso'] + ' hasta las ' + str(data[i]['Hora_salida'])))
+        api.update_status('Reunión presidencial N° #' +str([i]+1) + ' - ' + str(data[i]['Fecha']) + '\n' +  'El presidente Pedro Castillo Terrones se reunió hoy con ' + str(data[i]['Visitante'].title()) + ', ' + str(data[i]['Entidad']) + '.' + '\n' + 'La reunión se llevó a cabo desde las ' + str(data[i]['Hora_ingreso'] + ' hasta las ' + str(data[i]['Hora_salida'])))
 
         print('La visita de ' + str(data[i]['Visitante']) + ' ha sido tuiteada.')
         print('\n')
