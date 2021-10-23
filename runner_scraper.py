a015aa692832f944b66818c4caa896b02d0e3963
FabrizioSulcaR <78000967+FabrizioSulcaR@users.noreply.github.com>
2021-10-23 12:59:54 -0500

    Update runner_scraper.py

--- runner_scraper.py
+++ runner_scraper.py
@@ -1,13 +1,8 @@
 from scraper import Scraper, Filter
 import time
 
-inicio = time.time()
 x = Scraper()
 y = Filter()
 
 x.compute()
 y.compute()
-
-final = time.time()
-print('Bases de datos actualizadas')
-print("TIEMPO DE EJECUCIÓN",final-inicio)
